1e476fce54f8f79318bea31b0ca549d184d97c72
iadhunnoba <juanpablo.beloso@streambe.com>
2024-09-21 00:36:30 -0300

    Fix sensorReading model

--- models/SensorReading.js
+++ models/SensorReading.js
@@ -2,26 +2,22 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config');
 
-const SensorReading = sequelize.define('sensor_data', {
+const SensorReading = sequelize.define('SensorReading', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
     },
-    temperature: {
-        type: DataTypes.FLOAT,
-    },
-    humidity: {
-        type: DataTypes.FLOAT,
-    },
-    motion_detected: {
+    temperature: {  // Cambia esto al nombre correcto de tu columna
         type: DataTypes.FLOAT,
     },
     timestamp: {
         type: DataTypes.DATE,
+        field: 'timestamp',  // Asegúrate de usar el nombre correcto de la columna
     },
-
-    timestamp: false, // Desactivar los timestamps
+}, {
+    timestamps: false, // Esto debería desactivar los timestamps
+    tableName: 'sensor_data', // Asegúrate de que el nombre de la tabla sea correcto
 });
 
 module.exports = SensorReading;
